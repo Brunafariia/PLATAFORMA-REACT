2a60750b3af9ab4064c3c9df50e68dee44186e57
Brunafariia <brunafaria.souza@gmail.com>
2023-03-24 13:42:56 -0300

    cadastro herois versão 2

--- src/pages/CadastroSuperHerois/CadastroSuperHerois.jsx
+++ src/pages/CadastroSuperHerois/CadastroSuperHerois.jsx
@@ -1,46 +1,49 @@
 // Atividade 1:
 
 // Atividade: Desenvolva um formulário de cadastro de super-heróis. Criar um formulário de cadastro de super-heróis que permita ao usuário inserir informações sobre um super-herói fictício. Para isso crie uma nova página. O formulário deve conter campos para a entrada de nome, poder, fraqueza e história. Utilize os recursos do React Hook Form para obter os dados passados e as mostre em uma caixa de dialogo (alert). Não se preocupe com validação dos dados! Para isso crie uma nova página.
-
+import { useState } from "react";
+import { Alert } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 export function CadastroSuperHerois () {
 
     const { register, handleSubmit } = useForm();
+    const [heroi, setHeroi] = useState();
 
-    function onSubmitSuperHerois(heroi) {
-        alert(`
-        nome: ${heroi.nome}
-        poder: ${heroi.poder}
-        fraqueza: ${heroi.fraqueza}
-        historia: ${heroi.historia}
-        companhia:  ${heroi.companhia}`  
-        );
+    function onSubmitHeroi(heroi) {
+        setHeroi(heroi)
     }
 
     return (
-        <div className="superherois">
+        <div className="herois">
             <h1>Cadastro de Super Heróis</h1>
-            <form onSubmit={handleSubmit(onSubmitSuperHerois)} noValidate>
-                <label htmlFor="nome">Nome do Super Herói</label><br />
-                <input type="text" id="nome" {...register("nome")} /><br />
-                
-                <label htmlFor="poder">Poder</label><br />
-                <input type="text" id="poder" {...register("poder")} /><br />
-                
-                <label htmlFor="fraqueza">Fraqueza</label><br />
-                <input type="text" id="fraqueza" {...register("fraqueza")} /><br />
-                
-                <label htmlFor="historia">História</label><br />
-                <input type="text" id="historia" {...register("historia")} /><br />
-                
-                <label htmlFor="companhia">Companhia</label><br />
-                <input type="text" id="companhia" {...register("companhia")} /><br />
-                
-                <hr />
-                <button type="submit">Cadastrar Herói</button>
-                
+            <form onSubmit={handleSubmit(onSubmitHeroi)}>
+                <label htmlFor="nome">Nome</label> <br />
+                <input type="text" id="nome" {...register("nome")} /> <br />
+
+                <label htmlFor="poder">Poder</label> <br />
+                <input type="text" id="poder" {...register("poder")} /> <br />
+
+                <label htmlFor="fraqueza">Fraqueza</label> <br />
+                <input type="text" id="fraqueza" {...register("fraqueza")} /> <br />
+
+                <label htmlFor="historia">História</label> <br />
+                <textarea cols="23" id="historia" {...register("historia")}></textarea> <br />
+
+                <button type="submit">Cadastrar Heroi</button>
             </form>
+
+            <br />
+
+            {
+                heroi &&
+                <Alert variant="primary">
+                    Nome: {heroi.nome} <br />
+                    Poder: {heroi.poder} <br />
+                    Fraqueza: {heroi.fraqueza} <br />
+                    História: {heroi.historia} <br />
+                </Alert>
+            }
         </div>
     );
 }
